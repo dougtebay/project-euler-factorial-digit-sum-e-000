73d5cb165673409dd562f9dfe5d9d5f74b6b7a8d
dougtebay <dougtebay@gmail.com>
2015-12-17 10:02:17 -0500

    All tests passing

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,26 @@
 # code your solution here
+def factorial(num)
+  sum = 1
+  (2..num).each do |i|
+    sum *= i
+  end
+  sum
+end
+
+def sum_of_digits(num)
+  sum = 0
+  num = num.to_s.split("")
+  num.each do |i|
+    sum += i.to_i
+  end
+  sum
+end
+
+def factorial_digit_sum(num)
+  sum = 0
+  num = factorial(num).to_s.split("")
+  num.each do |i|
+    sum += i.to_i
+  end
+  sum
+end
